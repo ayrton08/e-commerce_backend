5da74586962f280368892465c5e59dd4c072a1b5
Ayrton <ayrtonjuarez90@gmail.com>
2022-10-27 14:59:45 -0300

    feat: api/auth/me/address - PATCH endpoint

---
+++ pages/api/me/address.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import method from "micro-method-router";
+
+import { authMiddleware } from "lib/middlewares";
+import { User } from "models/User";
+
+async function patch(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  token: { userId: string }
+) {
+  const { address } = req.body;
+
+  const user = new User(token.userId);
+
+  await user.pull();
+  
+  user.data.address = {
+    ...user.data.address,
+    ...address,
+  };
+  user.push();
+
+  res.status(200).send(user.data);
+}
+
+const handler = method({
+  patch,
+});
+
+export default authMiddleware(handler);
