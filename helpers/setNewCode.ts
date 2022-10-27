1640a26cb2c91a6255846a3d29bb31298a9d1e60
Ayrton <ayrtonjuarez90@gmail.com>
2022-10-27 14:07:03 -0300

    feat: api/auth/me - GET endpoint

--- helpers/setNewCode.ts
+++ helpers/setNewCode.ts
@@ -2,11 +2,11 @@ import { addMinutes } from "date-fns";
 import gen from "random-seed";
 import { Auth } from "models/Auth";
 
-const seed = "my secret";
+const seed = "a simple word";
 const random = gen.create(seed);
 
-export const setNewCode = async (auth: Auth) => {
-  const code = random.intBetween(10000, 99999);
+export const setNewCode = async (auth: Auth): Promise<number> => {
+  const code: number = random.intBetween(10000, 99999);
   const now = new Date();
   const twentyMinutesFromNow = addMinutes(now, 20);
   auth.data.code = code;
--- pages/api/me/index.ts
+++ pages/api/me/index.ts
@@ -1,8 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import method from "micro-method-router";
+
 import { authMiddleware } from "lib/middlewares";
 import { User } from "models/User";
-import type { NextApiRequest, NextApiResponse } from "next";
 
-async function handler(
+async function get(
   req: NextApiRequest,
   res: NextApiResponse,
   token: { userId: string }
@@ -10,7 +12,30 @@ async function handler(
   const user = new User(token.userId);
   await user.pull();
 
-  res.send(user.data);
+  res.status(200).send(user.data);
 }
+
+async function patch(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  token: { userId: string }
+) {
+  const newData = req.body;
+
+  const user = new User(token.userId);
+  await user.pull();
+  user.data = {
+    ...user.data,
+    ...newData,
+  };
+  user.push();
+
+  res.status(200).send(user.data);
+}
+
+const handler = method({
+  get,
+  patch,
+});
 
 export default authMiddleware(handler);
