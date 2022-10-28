0969c1d2dd5b2a60c0155dc2e9505201e490d371
Ayrton <ayrtonjuarez90@gmail.com>
2022-10-28 16:21:13 -0300

    feat(lib): Environment Variables

--- lib/algolia.ts
+++ lib/algolia.ts
@@ -2,7 +2,7 @@ import algoliasearch from "algoliasearch";
 
 const client = algoliasearch(
   process.env.ALGOLIA_APP_ID,
-  "d8f6f8fe77d7d40575098f1d0f0b1571"
+  process.env.ALGOLIA_API_KEY
 );
 
 export const products = client.initIndex("products");
