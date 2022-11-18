bf5e9d5d58042881d7ac7bc87579560301ed9974
Ayrton <ayrtonjuarez90@gmail.com>
2022-11-18 15:30:29 -0300

    fix: importations controllers

--- pages/api/sync/index.ts
+++ pages/api/sync/index.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getOffsetAndLimit } from "helpers/requests";
-import { syncAlgoliaWithAirtable } from "controllers/product.controller";
+import { syncAlgoliaWithAirtable } from "controllers/product-controller";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   const { limit } = getOffsetAndLimit(req, 100, 1000);
